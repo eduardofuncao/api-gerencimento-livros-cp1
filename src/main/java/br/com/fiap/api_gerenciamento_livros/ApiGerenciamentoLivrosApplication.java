7c0311e1134ba1c7bfca0a2583fd51e1aa1b6493
eduardofuncao <eduardofuncao@hotmail.com>
2024-09-10 09:07:21 -0300

    inicia projeto spring

---
+++ src/main/java/br/com/fiap/api_gerenciamento_livros/ApiGerenciamentoLivrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.api_gerenciamento_livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciamentoLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciamentoLivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/api_gerenciamento_livros/ApiGerenciamentoLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.api_gerenciamento_livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGerenciamentoLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
